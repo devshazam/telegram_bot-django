0163d69bb805b313a622ee0e2a0534ce44ab0370
shazam-dev <9956221@gmail.com>
2024-07-10 14:23:37 +0300

    2

--- my_tennis_club/members/admin.py
+++ my_tennis_club/members/admin.py
@@ -6,5 +6,9 @@ from .models import Clients
 class ClientsAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'id')
     search_fields = ['name', 'email', 'id']
-	
+
+    def my_function(self, obj) :
+        return self.stock
+    my_function.short_description = 'This is the Column Name'
+    my_function.allow_tags = True
 admin.site.register(Clients, ClientsAdmin)
