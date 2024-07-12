0c6d27d89fee31e2d131fddbd2234fe8258b8c0a
shazam-dev <9956221@gmail.com>
2024-07-12 17:19:38 +0300

    12

--- my_tennis_club/members/admin.py
+++ my_tennis_club/members/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Clients, Journal
+from .models import Clients, Journal, Errors
 # Register your models here.
 # admin.site.register(Clients)
 
@@ -11,3 +11,4 @@ class ClientsAdmin(admin.ModelAdmin):
 
 admin.site.register(Clients, ClientsAdmin)
 admin.site.register(Journal)
+admin.site.register(Errors)
